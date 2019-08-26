c910570ec65e059c0a342f983dfe239b71ec334e
yulf <cms17@wosign.com>
2019-08-26 14:10:50 +0800

    消息过滤器

---
+++ Frankyu.WebApiDemo/Frankyu.WebApiSample/App_Start/MyConstraints.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http.Routing;
+
+namespace Frankyu.WebApiSample
+{
+    public class MyConstraints : IHttpRouteConstraint
+    {
+        public bool Match(HttpRequestMessage request, IHttpRoute route, string parameterName,
+            IDictionary<string, object> values, HttpRouteDirection routeDirection)
+        {
+            object value;
+            if (values.TryGetValue(parameterName, out value) && value != null)
+            {
+                long longValue;
+                if (value is long)
+                {
+                    longValue = (long)value;
+                    return longValue != 0;
+                }
+
+                string valueString = Convert.ToString(value, CultureInfo.InvariantCulture);
+                if (Int64.TryParse(valueString, NumberStyles.Integer,
+                    CultureInfo.InvariantCulture, out longValue))
+                {
+                    return longValue != 0;
+                }
+            }
+            return false;
+        }
+
+    }
+}
--- Frankyu.WebApiDemo/Frankyu.WebApiSample/App_Start/WebApiConfig.cs
+++ Frankyu.WebApiDemo/Frankyu.WebApiSample/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 
 namespace Frankyu.WebApiSample
 {
@@ -11,14 +13,24 @@ namespace Frankyu.WebApiSample
         {
             // Web API configuration and services
 
+
+            // Create a message handler chain with an end-point.
+            var routeHandlers = HttpClientFactory.CreatePipeline(
+                new HttpControllerDispatcher(config), new[] { new ApiKeyHandler("ping") });
+
             // Web API routes
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
+                defaults: new { id = RouteParameter.Optional },
+                constraints: null,//该参数是什么?约束？
+                handler: routeHandlers
             );
+
+            //添加全局 消息处理器（MessageHandler）会在收到请求时就对该消息请求进行验证
+            //config.MessageHandlers.Add(new ApiKeyHandler("frank"));
         }
     }
 }
