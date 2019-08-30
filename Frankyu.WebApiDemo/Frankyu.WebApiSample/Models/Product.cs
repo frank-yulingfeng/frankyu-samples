35ad1429b9cda3fd6291eff2f1120701c61388a8
yulf <cms17@wosign.com>
2019-08-30 17:15:00 +0800

    .net framework api 传参验证

--- Frankyu.WebApiDemo/Frankyu.WebApiSample/Models/Product.cs
+++ Frankyu.WebApiDemo/Frankyu.WebApiSample/Models/Product.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Frankyu.WebApiSample.Models
 {
@@ -9,10 +10,12 @@ namespace Frankyu.WebApiSample.Models
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "{0} 必须填写")]
         public string Name { get; set; }
 
         public string Category { get; set; }
 
+        [Range(typeof(decimal), "0", "10000", ErrorMessage = "{0}数值不在范围内")]
         public decimal Price { get; set; }
     }
 }
