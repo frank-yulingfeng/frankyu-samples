b2f65d7f9e1c8841fdc0cb700d262f4cf660838b
frankyu <yu.lf@foxmail.com>
2020-02-15 17:55:43 +0800

    add

---
+++ Frankyu.EFDemo/Frankyu.Model/Course.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frankyu.Model
+{
+    public class Course
+    {
+        public Course()
+        {
+            this.Students = new HashSet<Student>();
+        }
+
+        public int CourseId { get; set; }
+
+        public string CourseName { get; set; }
+
+        public virtual ICollection<Student> Students { get; set; }
+    }
+}
---
+++ Frankyu.EFDemo/Frankyu.Model/Student.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frankyu.Model
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string StudentName { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public decimal Height { get; set; }
+
+        public float Weight { get; set; }
+
+        public Gender Gender { get; set; }
+
+
+        //fully defined relationship
+        public int? GradeId { get; set; }
+
+        public virtual Grade Grade { get; set; }
+        public virtual StudentAddress Address { get; set; }
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+
+    public enum Gender
+    {
+        Male,
+        Female
+    }
+}
